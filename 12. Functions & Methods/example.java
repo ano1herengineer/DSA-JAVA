4fea821ff098aa1eeba992e9614bded581eef9b3
ano1herengineer <singhprashantyolo@gmail.com>
2023-03-19 17:06:29 +0530

    test

---
+++ 12. Functions & Methods/example.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+
+public class example
+{
+    public static void main(String args[])
+    {
+        int i;
+        for(i =1; i<6 ; i++)
+        {
+            if(i>3) continue;
+        }
+        System.out.println(i);
+    }
+}
